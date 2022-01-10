0af8101c893447971b879f23da481df4337db74c
Juan Chavez <juan@Juans-Air-12.home>
2022-01-10 03:09:15 -0800

    update template

--- app.js
+++ app.js
@@ -97,17 +97,17 @@ const promptProject = portfolioData => {
         default: true
       },
       {
-          type: 'input',
-          name: 'about',
-          message: 'Provide some information about yourself',
-          when: ({  confirmAbout  }) => {
-              if(confirmAbout){
-                return true;
-              }
-              else{
-                  return false;
-              }
-            }  
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself',
+        when: ({  confirmAbout  }) => {
+            if(confirmAbout){
+            return true;
+            }
+            else{
+                return false;
+            }
+        }  
       },
       {
         type: 'confirm',
@@ -133,18 +133,65 @@ const promptProject = portfolioData => {
     });
 };
 
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => { 
-        console.log(portfolioData)
-    })
+const mockData = {
+    name: 'Lernantino',
+    github: 'lernantino',
+    confirmAbout: true,
+    about:
+      'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+    projects: [
+      {
+        name: 'Run Buddy',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['HTML', 'CSS'],
+        link: 'https://github.com/lernantino/run-buddy',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskinator',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'HTML', 'CSS'],
+        link: 'https://github.com/lernantino/taskinator',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Taskmaster Pro',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+        link: 'https://github.com/lernantino/taskmaster-pro',
+        feature: false,
+        confirmAddProject: true
+      },
+      {
+        name: 'Robot Gladiators',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+        languages: ['JavaScript'],
+        link: 'https://github.com/lernantino/robot-gladiators',
+        feature: false,
+        confirmAddProject: false
+      }
+    ]
+};
 
-;
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     const pageHTML = generatePage(portfolioData);
 
-// const pageHTML = generatePage(name, github);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
 
-// fs.writeFile('index.html', pageHTML, err => {
-//     if(err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html.');
+    // });
 // });
+
+const pageHTML = generatePage(mockData);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+});
